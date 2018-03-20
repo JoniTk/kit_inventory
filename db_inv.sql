caf4a110f8ec69db1afd28476c8176491c5d804c
= <=>
2018-03-20 12:31:48 +0600

    db commit

---
+++ db_inv.sql
@@ -0,0 +1,249 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 20, 2018 at 07:30 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_inv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_category`
+--
+
+CREATE TABLE `tbl_category` (
+  `id` int(11) NOT NULL,
+  `category_name` varchar(255) NOT NULL,
+  `category_status` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_category`
+--
+
+INSERT INTO `tbl_category` (`id`, `category_name`, `category_status`, `user_id`) VALUES
+(2, 'Dal kahbo', 1, 1),
+(3, 'tv', 1, 1),
+(4, 'shofa', 1, 1),
+(5, 'washing machine', 1, 1),
+(6, 'iron machine', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_customer`
+--
+
+CREATE TABLE `tbl_customer` (
+  `id` int(11) NOT NULL,
+  `customer_code` varchar(255) NOT NULL,
+  `customer_name` varchar(255) NOT NULL,
+  `customer_address` text NOT NULL,
+  `customer_phone` varchar(24) NOT NULL,
+  `customer_email` varchar(255) NOT NULL,
+  `customer_notes` text NOT NULL,
+  `customer_status` int(1) NOT NULL,
+  `login_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_customer`
+--
+
+INSERT INTO `tbl_customer` (`id`, `customer_code`, `customer_name`, `customer_address`, `customer_phone`, `customer_email`, `customer_notes`, `customer_status`, `login_id`) VALUES
+(1, 'test_01', 'Test Islam', 'Test Address', '0102920983', 'test@test.com', 'test notes ', 1, 1),
+(3, '10001', 'Test house', 'house', '01822270789', 'admin@gmail.com', 'Here comes notes', 1, 1),
+(4, 'sadsad', 'sadasd', 'sadasd', 'asdasd', 'dsfs@g.com', 'sdfdsf', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_item`
+--
+
+CREATE TABLE `tbl_item` (
+  `id` int(11) NOT NULL,
+  `item_code` varchar(255) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `item_model` varchar(255) NOT NULL,
+  `item_brand` varchar(255) NOT NULL,
+  `item_category` int(11) NOT NULL,
+  `item_unit` int(11) NOT NULL,
+  `item_description` varchar(255) NOT NULL,
+  `item_saleprice` double NOT NULL,
+  `item_re_order` double NOT NULL,
+  `item_status` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_item`
+--
+
+INSERT INTO `tbl_item` (`id`, `item_code`, `item_name`, `item_model`, `item_brand`, `item_category`, `item_unit`, `item_description`, `item_saleprice`, `item_re_order`, `item_status`, `user_id`) VALUES
+(1, 'asd', 'asd', 'asd', 'asd', 3, 12, 'asd', 123, 12, 1, 1),
+(4, 'aaii', 'adsf12', '213', 'Waltin', 3, 12, 'ads', 1234, 12, 1, 1),
+(5, 'asdf', 'asdf', 'adsf', 'asdf', 5, 12, 'adfa', 123, 123, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_login`
+--
+
+CREATE TABLE `tbl_login` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `user_Fname` varchar(50) NOT NULL,
+  `user_Lname` varchar(50) NOT NULL,
+  `user_pass` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `gender` int(1) NOT NULL,
+  `dob` date NOT NULL,
+  `user_image` text NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_login`
+--
+
+INSERT INTO `tbl_login` (`id`, `username`, `user_Fname`, `user_Lname`, `user_pass`, `user_email`, `gender`, `dob`, `user_image`, `status`) VALUES
+(1, 'admin', 'John', 'Doe', 'password', 'admin@admin.com', 1, '2018-03-01', 'http://localhost/inventory/userimg/avatar.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_supplier`
+--
+
+CREATE TABLE `tbl_supplier` (
+  `id` int(11) NOT NULL,
+  `supplier_code` varchar(255) NOT NULL,
+  `supplier_name` varchar(100) NOT NULL,
+  `supplier_address` text NOT NULL,
+  `contact_person` varchar(100) NOT NULL,
+  `supplier_phone` varchar(24) NOT NULL,
+  `supplier_notes` text NOT NULL,
+  `supplier_status` int(1) NOT NULL,
+  `login_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_supplier`
+--
+
+INSERT INTO `tbl_supplier` (`id`, `supplier_code`, `supplier_name`, `supplier_address`, `contact_person`, `supplier_phone`, `supplier_notes`, `supplier_status`, `login_id`) VALUES
+(1, 'test_01', 'sdfdsf', 'sdfsd', 'fdsfdsf', 'dsfdsfsdf', 'dsfsdf', 1, 1),
+(2, 'test_02', 'dfgdfgdf', 'gdfgdf', 'gdfg', 'dfgdfg', 'update notes', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_unit`
+--
+
+CREATE TABLE `tbl_unit` (
+  `id` int(11) NOT NULL,
+  `unit_name` varchar(255) NOT NULL,
+  `unit_status` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_unit`
+--
+
+INSERT INTO `tbl_unit` (`id`, `unit_name`, `unit_status`, `user_id`) VALUES
+(12, 'kg Pics', 1, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_item`
+--
+ALTER TABLE `tbl_item`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_login`
+--
+ALTER TABLE `tbl_login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_supplier`
+--
+ALTER TABLE `tbl_supplier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_unit`
+--
+ALTER TABLE `tbl_unit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tbl_item`
+--
+ALTER TABLE `tbl_item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tbl_login`
+--
+ALTER TABLE `tbl_login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_supplier`
+--
+ALTER TABLE `tbl_supplier`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_unit`
+--
+ALTER TABLE `tbl_unit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
